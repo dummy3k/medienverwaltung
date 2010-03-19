e089970f49b6f7f184d52f86078537dd7a340d9b
dummy3k <l4711@gmx.net>
2010-03-19 19:31:00 +0100

    initial commit

---
+++ find_dvds.py
@@ -0,0 +1,24 @@
+import os
+from optfunc import optfunc
+
+def try_get_files(directory):
+    try:
+        return os.listdir(directory)
+    except OSError as ex:
+        return []
+
+def find_dvds(directory):
+    if directory[-1:] == '/':
+        directory = directory[:-1]
+
+    for item in try_get_files(directory):
+        sub_dir = "%s/%s" % (directory, item)
+        if os.path.isdir(sub_dir):
+            if "VIDEO_TS" in try_get_files(sub_dir):
+                print item
+            else:
+                find_dvds(sub_dir)
+
+if __name__ == '__main__':
+    optfunc.run(find_dvds)
+    #~ optfunc.main([find_dvds,])
