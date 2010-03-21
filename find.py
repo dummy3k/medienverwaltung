c14fd14faaf441d9b3c5c1797dc881d52e6857fd
dummy3k <l4711@gmx.net>
2010-03-21 11:28:52 +0100

    use optfunc for amazon search

--- find.py
+++ find.py
@@ -1,17 +1,32 @@
 import amazonproduct
 import sys
 import ConfigParser
+from optfunc import optfunc
 
-config = ConfigParser.ConfigParser()
-config.read('settings.conf')
+import helper as h
 
-api = amazonproduct.API(config.get('Amazon', 'AccessKeyID'),
-                        config.get('Amazon', 'SecretAccessKey'))
-#~ node = api.item_search('Books', Publisher='Terminator')
-node = api.item_search('DVD', Title=sys.argv[1])
-for index, item in enumerate(node.Items.Item):
-    try:
-        #~ print item.ItemAttributes.Title
-        print "%s:\t%s" % (index, unicode(item.ItemAttributes.Title))
-    except:
-        print "NOOOOOOO"
+def find(keywords, SearchIndex='DVD'):
+    config = ConfigParser.ConfigParser()
+    config.read('settings.conf')
+
+    api = amazonproduct.API(config.get('Amazon', 'AccessKeyID'),
+                            config.get('Amazon', 'SecretAccessKey'))
+    #~ node = api.item_search('Books', Publisher='Terminator')
+    node = api.item_search(SearchIndex, Title=sys.argv[1])
+    print "Idx\tASIN\t\tTitle"
+    for index, item in enumerate(node.Items.Item):
+        print "%s:\t%s:\t%s" % (index,
+                                item.ASIN,
+                                unicode(item.ItemAttributes.Title)[:70])
+
+
+        details = api.item_lookup(item.ASIN)
+        item_detail = details.Items.Item[0]
+        for link_item in item_detail.ItemLinks.ItemLink:
+            print link_item.Description #, link_item.URL
+        #~ h.ipython()()
+        #~ break
+
+if __name__ == '__main__':
+    optfunc.run(find)
+    #~ optfunc.main([find_dvds,])
