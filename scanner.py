28a95e340ecb16f4e182772a410312f85826ac94
dummy3k <l4711@gmx.net>
2010-03-25 09:32:27 +0100

    fix scanner.py

--- scanner.py
+++ scanner.py
@@ -9,14 +9,14 @@ while True:
     if user_input == 'exit':
         break
 
-    #~ user_input = "9783789132186"
-
+    if not user_input:
+        continue
+    
     config = ConfigParser.ConfigParser()
     config.read('settings.conf')
 
     api = amazonproduct.API(config.get('Amazon', 'AccessKeyID'),
                             config.get('Amazon', 'SecretAccessKey'))
-    #~ node = api.item_search('Books', Publisher='Terminator')
     SearchIndex='Books'
     node = api.item_lookup(user_input, IdType='ISBN', SearchIndex=SearchIndex)
     item = node.Items.Item
