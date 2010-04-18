ae969d68fdb8db2a98be763556bd22db61db90ae
dummy3k <l4711@gmx.net>
2010-04-18 15:13:30 +0200

    simple build script

---
+++ build_eggs.sh
@@ -0,0 +1,11 @@
+cd medienverwaltung-common/
+python2.5 setup.py bdist_egg
+python2.6 setup.py bdist_egg
+cp dist/*.egg /home/dummy/Dropbox/Public/py/
+
+cd ../medienverwaltungweb
+python2.5 setup.py bdist_egg
+python2.6 setup.py bdist_egg
+cp dist/*.egg /home/dummy/Dropbox/Public/py/
+
+cd ..
