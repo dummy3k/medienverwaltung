403d9dbe662f624e9d0bc704e5470e6bb606b497
dummy3k <l4711@gmx.net>
2010-04-24 23:18:48 +0200

    fix hudson script

--- build_eggs_hudson.sh
+++ build_eggs_hudson.sh
@@ -2,7 +2,7 @@ cd medienverwaltung-common/
 python2.5 setup.py bdist_egg || exit 1
 
 cd ../medienverwaltungweb
-python2.5 setup.py compile_catalog || exit 1
+#python2.5 setup.py compile_catalog || exit 1
 python2.5 setup.py bdist_egg || exit 1
 
 cd ../medienverwaltung_cli
