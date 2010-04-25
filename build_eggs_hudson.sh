7d852eb01abe7b5ae42d3a755d5447c5bfd54f9d
dummy3k <l4711@gmx.net>
2010-04-25 15:15:19 +0200

    create virtualenv for hudson

--- build_eggs_hudson.sh
+++ build_eggs_hudson.sh
@@ -1,8 +1,28 @@
+if [ -d "unittest.env" ];
+then
+    echo "Unittest environment exists"
+    do_setup="NO"
+else
+    echo "Creating unittest environment"
+    virtualenv unittest.env || exit 1
+    do_setup="YES"
+fi
+
+source unittest.env/bin/activate
+
 cd medienverwaltung-common/
+if [ $do_setup == "YES" ];
+then
+    python setup.py develop
+fi
 python2.5 setup.py bdist_egg || exit 1
 
 cd ../medienverwaltungweb
 #python2.5 setup.py compile_catalog || exit 1
+if [ $do_setup == "YES" ];
+then
+    python setup.py develop
+fi
 nosetests || exit 1
 python2.5 setup.py bdist_egg || exit 1
 
