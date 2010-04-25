db5ff7bafda5287d971a8a215342ad1ebf39c26c
dummy3k <l4711@gmx.net>
2010-04-25 21:48:00 +0200

    dont cover other modules

--- build_eggs_hudson.sh
+++ build_eggs_hudson.sh
@@ -1,3 +1,6 @@
+#~ echo `pwd`/medienverwaltung-common/medienverwaltungcommon
+#~ exit 0
+
 if [ -d "unittest.env" ];
 then
     echo "Unittest environment exists"
@@ -16,6 +19,7 @@ then
     python setup.py develop
 fi
 python2.5 setup.py bdist_egg || exit 1
+common_path=`pwd`
 
 cd ../medienverwaltungweb
 #python2.5 setup.py compile_catalog || exit 1
@@ -26,8 +30,10 @@ then
     python setup.py develop
 fi
 nosetests --with-xunit --with-coverage --cover-package=medienverwaltungweb || exit 1
-coverage html -i --omit=/usr/,data/,*/medienverwaltungcommon || exit 1
+coverage html -i --omit=/usr/,data/,$common_path || exit 1
 python2.5 setup.py bdist_egg || exit 1
 
 cd ../medienverwaltung_cli
 python2.5 setup.py bdist_egg || exit 1
+
+echo $common_path
