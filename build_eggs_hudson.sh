fef506dbc3cd53b482c1e173bc6e214bbb6f05e2
dummy3k <l4711@gmx.net>
2010-04-25 22:24:41 +0200

    really hide common in coverage

--- build_eggs_hudson.sh
+++ build_eggs_hudson.sh
@@ -1,5 +1,6 @@
 #~ echo `pwd`/medienverwaltung-common/medienverwaltungcommon
 #~ exit 0
+common_path=`python where_is_common.py`
 
 if [ -d "unittest.env" ];
 then
@@ -19,7 +20,7 @@ then
     python setup.py develop
 fi
 python2.5 setup.py bdist_egg || exit 1
-common_path=`pwd`
+#~ common_path=`pwd`
 
 cd ../medienverwaltungweb
 #python2.5 setup.py compile_catalog || exit 1
