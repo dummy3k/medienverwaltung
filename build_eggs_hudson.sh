395aed53c62adbb2fd609e07e573fdbb5f69f1e5
dummy3k <l4711@gmx.net>
2010-04-25 14:27:05 +0200

    run tests on hudson build

--- build_eggs_hudson.sh
+++ build_eggs_hudson.sh
@@ -3,6 +3,7 @@ python2.5 setup.py bdist_egg || exit 1
 
 cd ../medienverwaltungweb
 #python2.5 setup.py compile_catalog || exit 1
+nosetests || exit 1
 python2.5 setup.py bdist_egg || exit 1
 
 cd ../medienverwaltung_cli
