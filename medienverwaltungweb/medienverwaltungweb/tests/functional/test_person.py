65bd9c2f03e6fe109583ef9c96d6ba76a296f49e
dummy3k <l4711@gmx.net>
2010-04-30 21:38:50 +0200

    person controller unittests

--- medienverwaltungweb/medienverwaltungweb/tests/functional/test_person.py
+++ medienverwaltungweb/medienverwaltungweb/tests/functional/test_person.py
@@ -29,6 +29,17 @@ class TestPersonController(TestController):
         record = meta.Session.query(model.Person).get(1)
         self.assertEqual('Changed', record.name)
 
+    def test_edit_post_alias(self):
+        response = self.app.get(url(controller='person',
+                                    action='edit_post',
+                                    id='1'),
+                                 params={'name':'Changed',
+                                         'create_alias':'yes'})
+
+        record = meta.Session.query(model.Person).get(1)
+        self.assertEqual('Changed', record.name)
+        self.assertEqual(1, len(record.aliases))
+
 
     def test_list(self):
         record = model.Person()
@@ -41,4 +52,29 @@ class TestPersonController(TestController):
 
         assert "Bruce Schneier" in response
         assert "Chuck Norries" in response
+
+    def test_top_ten(self):
+        medium = model.Medium()
+        medium.title = u"A Medium"
+        meta.Session.add(medium)
+
+        relation = model.RelationType()
+        relation.name = u"Author"
+        meta.Session.add(relation)
+
+        record = model.PersonToMedia()
+        bruce = meta.Session.query(model.Person).get(1)
+        bruce.persons_to_media.append(record)
+        medium.persons_to_media.append(record)
+        relation.persons_to_media.append(record)
+        meta.Session.add(record)
+
+        meta.Session.commit()
+
+        response = self.app.get(url(controller='person',
+                                    action='top_ten'))
+
+        assert "Bruce Schneier" in response
+        assert "Author" in response
+        assert not "Actor" in response
 
